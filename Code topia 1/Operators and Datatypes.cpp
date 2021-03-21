ce33aeb173972cd82d4c019ad88a0feb365f6841
Tabish Khalid Halim <tkhalim14@gmail.com>
2021-03-21 16:54:41 +0500

    Add files via upload

---
+++ Code topia 1/Operators and Datatypes.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+// This is a comment {It doesn't execute with your code} .
+int main()
+{
+    cout<<"Whazzup people!!!";
+    // HOW TO DECLARE A VARIABLE {variables}
+    // Datatype variable_name ;
+    int a; // This is an Integer datatype . Size = 4
+    char b; // This is a Character{ Alphabets,special symbols including spaces e.g. @ % # $ - _}
+    long int j ; // This is also Integer type but for longer digits Integers
+    float s; //Stores decimal type values e.g. 3.14
+    double t ; // Stores even longer decimals
+    // OPERATORS : + , - , * , / , %
+    //cout<<a+5<<endl; //This is addition endl = end of line{shifts the end of output to new line}
+    //s = 14.05 ;
+    //cout<< 3.14*s<<"\n"; // '\n' is an alternative for endl i.e. next line
+    //cout<<5%2<<endl;
+    return 0;
+}
